15c09f3008ab67de307a60b4ee62c0c61f1a735c
Hampus Ram <hampus.ram@gmail.com>
2023-07-04 19:18:58 +0200

    deep sleep more

--- client/main.py
+++ client/main.py
@@ -9,9 +9,21 @@ import struct
 
 # How often to measure in seconds
 PERIOD = const(15 * 60)
-
+# How early we need to wake up to be certain to connect,
+# set the time and get ready to measure.
+BUFFER_TIME = const(20)
 
 msg_buffer = bytearray(6)
+
+
+def deep_sleep(time_ms):
+    print("going into deep sleep")
+
+    rtc = machine.RTC()
+    rtc.irq(trigger=rtc.ALARM0, wake=machine.DEEPSLEEP)
+    rtc.alarm(rtc.ALARM0, time_ms)
+
+    machine.deepsleep()
 
 
 def read_config():
@@ -26,7 +38,12 @@ def publish(client, topic, timestamp, temp, hum):
 
 def wait_for_period(early=0):
     seconds_to_period = PERIOD - (time() % PERIOD) - early
-    if seconds_to_period > 0:
+    # deep sleep does not seem to be that exact so let's not
+    # use it if we need to hit the target with some precision
+    if seconds_to_period > BUFFER_TIME and early > 0:
+        print(f"deep sleeping for {seconds_to_period} seconds (early = {early})")
+        deep_sleep(seconds_to_period * 1000)
+    elif seconds_to_period > 0:
         print(f"sleeping for {seconds_to_period} seconds (early = {early})")
         sleep(seconds_to_period)
 
@@ -54,15 +71,15 @@ def perform_measurement(client_id, server, topic, dht):
 def main():
     ntptime.settime()
 
+    # wait until it is almost time to measure
+    wait_for_period(early=BUFFER_TIME)
+
     config = read_config()
 
     client_id = hexlify(machine.unique_id())
     server = config["server"]
     topic = config["topic"] + "/" + client_id.decode("UTF-8")
     dht = sensor.create(config["model"], config["dhtPin"])
-
-    # wait until it is almost time to measure
-    wait_for_period(early=60)
 
     try:
         perform_measurement(client_id, server, topic, dht)
@@ -73,15 +90,9 @@ def main():
 
     # Sleep for slightly shorter than the period. To save power and
     # have some slack to be able to connect to the network, mqtt etc.
-    # upon reset
+    # upon reset. Long deep sleeps seem to be quite inexact.
 
-    print("going into deep sleep")
-
-    rtc = machine.RTC()
-    rtc.irq(trigger=rtc.ALARM0, wake=machine.DEEPSLEEP)
-    rtc.alarm(rtc.ALARM0, (PERIOD - 90) * 1000)
-
-    machine.deepsleep()
+    deep_sleep((PERIOD - 2 * BUFFER_TIME) * 1000)
 
 
 if __name__ == "__main__":
